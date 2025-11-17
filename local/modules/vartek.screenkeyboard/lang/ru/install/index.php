2bfd96cda921cdffa1eb9efea1629cf6a87de93e
evartanyan1989 <evartanyan1989@gmail.com>
2025-11-17 17:51:45 +0800

    Edited local/modules/vartek.screenkeyboard/lang/ru/install/index.php

--- local/modules/vartek.screenkeyboard/lang/ru/install/index.php
+++ local/modules/vartek.screenkeyboard/lang/ru/install/index.php
@@ -1,13 +1,13 @@
 <?php
-$MESS['VSK_MODULE_NAME'] = 'Экранная клавиатура';
-$MESS['VSK_MODULE_DESC'] = 'Экранная клавиатура';
+$MESS['VSK_MODULE_NAME'] = 'Экранная клавиатура (Screen Keyboard)';
+$MESS['VSK_MODULE_DESC'] = 'Модуль подключает адаптивную экранную клавиатуру для всех полей ввода. Поддержка раскладок (RU/EN/NUM/EMAIL)';
 
-$MESS['VSK_INSTALL_TITLE'] = 'Экранная клавиатура';
-$MESS['VSK_INSTALL_SUCCESS'] = 'Экранная клавиатура';
-$MESS['VSK_INSTALL_GO_TO_SETTINGS'] = 'Экранная клавиатура';
-$MESS['VSK_INSTALL_BTN_CONTINUE'] = 'Экранная клавиатура';
+$MESS['VSK_INSTALL_TITLE'] = 'Установка модуля «Экранная клавиатура (Screen Keyboard)»';
+$MESS['VSK_INSTALL_SUCCESS'] = 'Модуль успешно установлен.';
+$MESS['VSK_INSTALL_GO_TO_SETTINGS'] = 'Для настройки модуля перейдите в раздел настроек.';
+$MESS['VSK_INSTALL_BTN_CONTINUE'] = 'Продолжить';
 
-$MESS['VSK_UNINSTALL_TITLE'] = 'Экранная клавиатура';
-$MESS['VSK_UNINSTALL_CONFIRM'] = 'Экранная клавиатура';
-$MESS['VSK_UNINSTALL_SAVE_DATA'] = 'Экранная клавиатура';
-$MESS['VSK_UNINSTALL_BTN_CONTINUE'] = 'Экранная клавиатура';
+$MESS['VSK_UNINSTALL_TITLE'] = 'Удаление модуля «Экранная клавиатура (Screen Keyboard)»';
+$MESS['VSK_UNINSTALL_CONFIRM'] = 'Вы уверены, что хотите удалить модуль?';
+$MESS['VSK_UNINSTALL_SAVE_DATA'] = 'Сохранить данные и настройки модуля';
+$MESS['VSK_UNINSTALL_BTN_CONTINUE'] = 'Удалить';
