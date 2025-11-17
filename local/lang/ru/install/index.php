4d993f84915633beee07bcb3bcf21920e8bf99c5
evartanyan1989 <evartanyan1989@gmail.com>
2025-11-17 16:23:23 +0800

    screenkeyboard — added local/modules/vartek.screenkeyboard/install/index.php:__construct

---
+++ local/lang/ru/install/index.php
@@ -0,0 +1,3 @@
+<?php
+$MESS['VSK_MODULE_NAME'] = 'Экранная клавиатура';
+$MESS['VSK_MODULE_DESC'] = 'Экранная клавиатура';
--- local/modules/vartek.screenkeyboard/install/index.php
+++ local/modules/vartek.screenkeyboard/install/index.php
@@ -1,9 +1,22 @@
 <?php
 
+use Bitrix\Main\Localization\Loc;
+
 class vartek_screenkeyboard extends CModule
 {
+    public $MODULE_ID = 'vartek.screenkeyboard';
+    public $MODULE_VERSION;
+    public $MODULE_VERSION_DATE;
+    public $MODULE_NAME;
+    public $PARTNER_NAME = "Vartek";
+    public $PARTNER_URI  = "";
     public function __construct()
     {
-        
+        include __DIR__ . '/version.php';
+
+        $this->MODULE_VERSION      = $arModuleVersion['VERSION'];
+        $this->MODULE_VERSION_DATE = $arModuleVersion['VERSION_DATE'];
+        $this->MODULE_NAME         = Loc::getMessage('VSK_MODULE_NAME');
+        $this->MODULE_DESCRIPTION  = Loc::getMessage('VSK_MODULE_DESC');
     }
 }
